4008a3838b43e66fa209c1f40fa3fe36e30b33f7
akagiyuu <khanghuynh0245@gmail.com>
2026-05-06 18:52:10 +0700

    feat(renderer): add constructor

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,25 @@ use futures::StreamExt;
 pub struct Renderer {
     pub browser: Browser,
 }
+
+impl Renderer {
+    pub async fn new() -> chromiumoxide::error::Result<Self> {
+        let (browser, mut handler) = Browser::launch(
+            BrowserConfig::builder()
+                .new_headless_mode()
+                .build()
+                .expect("config for headless browser must be valid"),
+        )
+        .await?;
+
+        tokio::spawn(async move {
+            while let Some(h) = handler.next().await {
+                if h.is_err() {
+                    break;
+                }
+            }
+        });
+
+        Ok(Self { browser })
+    }
+}
